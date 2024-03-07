d26fff41192fcf8424acba0eabac66c9d2f97668
jhevtech <jhevaughnpanton.17@gmail.com>
2024-03-06 19:50:44 -0800

    changes

--- temperatureconversion.cpp
+++ temperatureconversion.cpp
@@ -19,17 +19,24 @@ int main(){
             cin >> temp;
             temp = (temp * 1.8) + 32.0;
             cout << "temperature is: " << temp << "F" << endl;
-        }
+            }
+        else if(temp == char()){
+            cout << "Invalid input. Please enter a number." << endl; 
+                 
+            
+            
+            }
         else if (unit == 'C' || unit == 'c'){
             cout << "Enter the temperature in Fahrenheit: ";
             cin >> temp;
             temp = (temp - 32.0) / 1.8;
             cout << "temperature is: " << temp << "C" << endl;
-        }
+            }
         else{
             cout << "Please enter C or F" << endl;
-        }
-    }
+            } 
+        
+    } 
 
 return 0;
 }
